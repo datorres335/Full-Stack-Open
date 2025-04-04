96ea0adc495e851c372ce0c7af3e82ff4141a15d
datorres335 <datorres335@gmail.com>
2025-04-03 21:46:12 -0700

    implemented info page

--- part3/index.js
+++ part3/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json()) //used to convert json data into a javascript object, used in post requests
 
 let persons = [
     { 
@@ -21,8 +22,23 @@ let persons = [
       "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    { 
+        "id": "5",
+        "name": "Test Delete", 
+        "number": "999-99-9989"
     }
 ]
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    const numOfPeople = `Phonebook has info for ${persons.length} people`
+    response.send(
+        `<p>${numOfPeople}<p/>
+        <p>${date}</p>`
+    )
+    //response.send('<h1>Whats up world?</h1>')
+})
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -30,6 +46,49 @@ app.get('/', (request, response) => {
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
+})
+
+//this allows users to get the info of a specific person by their id
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generatedId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
+
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    const person = {
+        id: generatedId(),
+        name: body.name,
+        number: body.number
+    }
+    notes = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
