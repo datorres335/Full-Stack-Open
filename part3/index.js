45e41579e5f4c65b82218329174da49d5051adae
datorres335 <datorres335@gmail.com>
2025-04-03 22:02:15 -0700

    implemented http post request with random number generator for id

--- part3/index.js
+++ part3/index.js
@@ -66,9 +66,7 @@ app.delete('/api/persons/:id', (request, response) => {
 })
 
 const generatedId = () => {
-    const maxId = persons.length > 0
-        ? Math.max(...persons.map(n => Number(n.id)))
-        : 0
+    const maxId = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 6 + 1)) + 6;
 
     return String(maxId + 1)
 }
