bd04525158239e0c213514e66e879a57725d8809
datorres335 <datorres335@gmail.com>
2025-04-07 17:56:41 -0700

    able to update person's phone number if name already exists

--- part3/index.js
+++ part3/index.js
@@ -123,6 +123,27 @@ app.post('/api/persons', (request, response, next) => {
         .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const {name, number} = request.body
+
+    Person.findByIdAndUpdate(request.params.id)
+        .then(person => {
+            if (!persons) {
+                return response.status(404).end()
+            }
+
+            //person.name = name
+            person.number = number
+
+            return person
+                .save()
+                .then(updatedPerson => {
+                    response.json(updatedPerson)
+                })
+        })
+        .catch(error => next(error))
+})
+
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
