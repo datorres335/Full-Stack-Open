ce423e4932ec1b853075c0375acff644916eed5e
datorres335 <datorres335@gmail.com>
2025-04-07 21:18:17 -0700

    implemented validation for phone number format

--- part3/models/person.js
+++ part3/models/person.js
@@ -18,7 +18,17 @@ const personSchema = new mongoose.Schema({
         minLength: 3,
         required: true
     },
-    number: String,
+    number: {
+        type: String,
+        minLength: 8,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number. Must be in the format XX-XXXXXXX or XXX-XXXXXXX`
+        },
+        required: true
+    },
 })
 
 personSchema.set('toJSON', {
