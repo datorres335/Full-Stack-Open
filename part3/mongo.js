f7d9323aa31180acd8c6525ba167cfbb73c1a2bd
datorres335 <datorres335@gmail.com>
2025-04-08 21:02:56 -0700

    added ESlint to project and fixed errors

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,13 +1,13 @@
 /*
     NOTE!!!!!!!!!
-    THIS IS SAMPLE CODE FOR MONGODB USAGE, 
+    THIS IS SAMPLE CODE FOR MONGODB USAGE,
     refer to person.js for actual project implementation of mongoDB
 */
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -18,36 +18,36 @@ mongoose.set('strictQuery', false) // this is to avoid the deprecation warning f
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema) // this creates a model called Person based on the personScheme, will show up as "people" in MongoDB Atlas (MongoDB pluralizes the model name)
 
 if (process.argv.length === 3) {
-    Person.find({}).then(result => {
-        console.log('phonebook:');        
-        result.forEach(person => {
-            //console.log(person)
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      //console.log(person)
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 } else if (process.argv.length === 5) {
-    const person = new Person({
-        name: `${process.argv[3]}`,
-        number: `${process.argv[4]}`,
-    })
+  const person = new Person({
+    name: `${process.argv[3]}`,
+    number: `${process.argv[4]}`,
+  })
 
-    person.save().then(result => {
-        console.log('person saved!')
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
 } else {
-    console.log('Invalid number of arguments.')
+  console.log('Invalid number of arguments.')
 }
 /*
     NOTE!!!!!!!!!
-    THIS IS SAMPLE CODE FOR MONGODB USAGE, 
+    THIS IS SAMPLE CODE FOR MONGODB USAGE,
     refer to person.js for actual project implementation of mongoDB
 */
